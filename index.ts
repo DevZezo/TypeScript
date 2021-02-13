8a79ffb5226e79fc42d6b436f692910ee476e9db
DevZezo <74310751+DevZezo@users.noreply.github.com>
2021-02-14 02:31:37 +0300

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,12 @@
+const info = {
+    name: "Zezo",
+    age: "15",
+    programming_languages: "HTML, CSS, SCSS, JS, C# & PYTHON",
+    from: "iraq"
+}
+
+
+console.log(info.name)
+console.log(info.age)
+console.log(info.programming_languages)
+console.log(info.from)
